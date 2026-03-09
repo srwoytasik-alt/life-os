245cdefc8b6f7443f1d280ecf9efb8d16ec2e0b1
Steven Woytasik <your-email@example.com>
2026-03-09 06:41:09 -0500

    Fix stale DB connections with pool_pre_ping

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SQLALCHEMY_ENGINE_OPTIONS = {
-        "pool_pre_ping": True
+        "pool_pre_ping": True,
+        "pool_recycle": 280
     }
 
 
